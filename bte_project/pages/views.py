a532db36f0c728231e0116b82afd4c74a68e49b0
Ozal Khan <ozalkhan196@gmail.com>
2018-12-19 13:36:34 -0500

    realtors are dynamically displayed

--- bte_project/pages/views.py
+++ bte_project/pages/views.py
@@ -5,10 +5,26 @@ from django.http import HttpResponse
 # some sort of render/ routing displays html
 # upon request
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(req):
-    return render(req, 'pages/index.html')
+    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(req, 'pages/index.html', context)
 
 
 def about(req):
-    return render(req, 'pages/about.html')
+    # get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(req, 'pages/about.html', context)
